d776745186ca02b72c6e72ec80015c706540a96c
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2022-04-10 08:42:20 +0300

    add React project template

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,83 +1,17 @@
-import React, { Component } from 'react';
-import { nanoid } from 'nanoid';
-import { ContactForm } from './ContactForm';
-import ContactList from './ContactList';
-import Filter from './Filter';
-
-class App extends Component {
-  state = {
-    contacts: [],
-    filter: '',
-  };
-
-  componentDidMount() {
-    const contacts = localStorage.getItem('Contacts');
-    const parsedContacts = JSON.parse(contacts);
-    if (parsedContacts) {
-      this.setState({ contacts: parsedContacts });
-    }
-  }
-  componentDidUpdate(prevProps, prevState) {
-    if (prevState.contact !== this.state.contacts) {
-      localStorage.setItem('Contacts', JSON.stringify(this.state.contacts));
-    }
-  }
-
-  addContact = ({ name, number }) => {
-    const { contacts } = this.state;
-    for (const contact of contacts) {
-      if (contact.name === name) {
-        alert(`${name} is already in contacts.`);
-        return;
-      }
-    }
-    const contact = {
-      id: nanoid(),
-      name,
-      number,
-    };
-    this.setState(({ contacts }) => ({
-      contacts: [contact, ...contacts],
-    }));
-  };
-
-  deleteContact = contactId => {
-    this.setState(prevState => ({
-      contacts: prevState.contacts.filter(contact => contact.id !== contactId),
-    }));
-  };
-
-  changeFilter = e => {
-    this.setState({
-      filter: e.currentTarget.value,
-    });
-  };
-
-  getVisibleContacts = () => {
-    const { contacts, filter } = this.state;
-    const normalizedFilter = filter.toLowerCase();
-    return contacts.filter(({ name }) =>
-      name.toLowerCase().includes(normalizedFilter)
-    );
-  };
-
-  render() {
-    const { filter } = this.state;
-    const visibleContacts = this.getVisibleContacts();
-    return (
-      <div className="container">
-        <h1>Phonebook</h1>
-        <ContactForm onSubmit={this.addContact} />
-
-        <h2>Contacts</h2>
-        <Filter value={filter} onChange={this.changeFilter} />
-        <ContactList
-          onDeleteContact={this.deleteContact}
-          contacts={visibleContacts}
-        />
-      </div>
-    );
-  }
-}
-
-export default App;
+export const App = () => {
+  return (
+    <div
+      style={{
+        height: '100vh',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        textTransform: 'uppercase',
+        color: '#010101',
+      }}
+    >
+      React homework template
+    </div>
+  );
+};
