bc9e0d8b67b5d622379addd9f0ec5b0de2feb433
Vladimir Rudnyk <86798079+VSRudnyk@users.noreply.github.com>
2022-04-12 18:36:06 +0300

    mentor's remarks corrected

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,79 @@
 import React, { Component } from 'react';
+import { Spinner } from 'components/Spinner/Spinner';
+import { Button } from 'components/Button/Button';
 import { Searchbar } from './Searchbar/Searchbar';
-import { ImageGallery } from './ImageGallery/ImageGallery';
+import { ImageGallery } from 'components/ImageGallery/ImageGallery';
+import photoAPI from './Api/fetchPhoto';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 export class App extends Component {
   state = {
     searchQuery: '',
+    photoCards: [],
+    page: 1,
+    status: 'idle',
+  };
+
+  LoadMorePhoto = () => {
+    this.setState(prevState => ({
+      page: prevState.page + 1,
+    }));
   };
 
   handleFormSubmit = searchQuery => {
     this.setState({ searchQuery });
   };
 
+  simpleLightbox = () => {
+    var lightbox = new SimpleLightbox('.gallery a', {});
+    lightbox.refresh();
+  };
+
+  componentDidUpdate(prevProps, prevState) {
+    this.simpleLightbox();
+    const { page, searchQuery } = this.state;
+
+    if (prevState.searchQuery !== searchQuery) {
+      this.setState({ photoCards: [] });
+    }
+    if (prevState.searchQuery !== searchQuery || prevState.page !== page) {
+      this.setState({ status: 'pending' });
+      photoAPI
+        .fetchPhoto(searchQuery, page)
+        .then(photoCards => {
+          this.setState(prevState => ({
+            photoCards: [...prevState.photoCards, ...photoCards.hits],
+            status: 'resolved',
+          }));
+        })
+        .catch(error => this.setState({ error, status: 'rejected' }));
+    }
+  }
+
   render() {
-    const { searchQuery } = this.state;
-    return (
-      <>
-        <Searchbar onSubmit={this.handleFormSubmit} />
-        <ImageGallery searchQuery={searchQuery} />
-      </>
-    );
+    const { status, photoCards } = this.state;
+
+    if (status === 'idle') {
+      return <Searchbar onSubmit={this.handleFormSubmit} />;
+    }
+
+    if (status === 'pending') {
+      return <Spinner />;
+    }
+
+    if (status === 'resolved') {
+      return (
+        <>
+          <Searchbar onSubmit={this.handleFormSubmit} />
+          <ImageGallery photoCards={photoCards} />
+          <Button onClick={this.LoadMorePhoto} />
+        </>
+      );
+    }
+
+    if (status === 'rejected') {
+      return;
+    }
   }
 }
--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,76 +1,17 @@
-import React, { Component } from 'react';
-import { Spinner } from 'components/Spinner/Spinner';
 import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
-import photoAPI from '../Api/fetchPhoto';
 import { List } from './ImageGallery.styled';
-import { Button } from 'components/Button/Button';
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
 import PropTypes from 'prop-types';
 
-export class ImageGallery extends Component {
-  state = {
-    photoCards: [],
-    page: 1,
-    status: 'idle',
-  };
-
-  LoadMorePhoto = () => {
-    this.setState(prevState => ({
-      page: prevState.page + 1,
-    }));
-  };
-
-  simpleLightbox = () => {
-    var lightbox = new SimpleLightbox('.gallery a', {});
-    lightbox.refresh();
-  };
-
-  componentDidUpdate(prevProps, prevState) {
-    this.simpleLightbox();
-    const { page } = this.state;
-    const { searchQuery } = this.props;
-    if (prevProps.searchQuery !== searchQuery) {
-      this.setState({ photoCards: [] });
-    }
-    if (prevProps.searchQuery !== searchQuery || prevState.page !== page) {
-      this.setState({ status: 'pending' });
-      photoAPI
-        .fetchPhoto(searchQuery, page)
-        .then(photoCards => {
-          this.setState(prevState => ({
-            photoCards: [...prevState.photoCards, ...photoCards.hits],
-            status: 'resolved',
-          }));
-        })
-        .catch(error => this.setState({ error, status: 'rejected' }));
-    }
-  }
-
-  render() {
-    const { status, photoCards } = this.state;
-
-    if (status === 'pending') {
-      return <Spinner />;
-    }
-
-    if (status === 'resolved') {
-      return (
-        <div className="gallery">
-          <List>
-            <ImageGalleryItem photoCards={photoCards} />
-          </List>
-          <Button onClick={this.LoadMorePhoto} />
-        </div>
-      );
-    }
-
-    if (status === 'rejected') {
-      return;
-    }
-  }
-}
+export const ImageGallery = ({ photoCards }) => {
+  return (
+    <div className="gallery">
+      <List>
+        <ImageGalleryItem photoCards={photoCards} />
+      </List>
+    </div>
+  );
+};
 
 ImageGallery.propTypes = {
-  searchQuery: PropTypes.string.isRequired,
+  photoCards: PropTypes.array.isRequired,
 };
